3e36abf17271d90bc9fcb05f1398bf760318af93
GerardoX1 <yeral_way@hotmail.com>
2016-08-21 16:50:08 -0500

    Update Ejercicio2.js

--- rv/Ejercicio2.js
+++ rv/Ejercicio2.js
@@ -8,7 +8,7 @@ renderizador.setSize( window.innerHeight*.95,
                       window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 var forma = new THREE.SphereGeometry( 2, 15, 15);
-var material = new THREE.MeshNormalMaterial( {color: 0xffff00} );
+var material = new THREE.MeshNormalMaterial( {color: 0x808000} );
 var sphere = new THREE.Mesh( forma, material );
 
 escena.add( sphere );
