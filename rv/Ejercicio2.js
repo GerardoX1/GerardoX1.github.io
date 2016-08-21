d104871a9a98476455cca4d551d1fc8fa0480a55
GerardoX1 <yeral_way@hotmail.com>
2016-08-21 16:53:02 -0500

    Update Ejercicio2.js

--- rv/Ejercicio2.js
+++ rv/Ejercicio2.js
@@ -1,15 +1,15 @@
 var escena = new THREE.Scene();
 var camara =new THREE.PerspectiveCamera();
 
-camara.position.z = 10;
-camara.position.y = -2;
+camara.position.z = 20;
+camara.position.y = -3;
 
 var renderizador= new THREE.WebGLRenderer();
 
 renderizador.setSize( window.innerHeight*.95,
                       window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
-var forma = new THREE.SphereGeometry( 2, 15, 15);
+var forma = new THREE.SphereGeometry( 3, 15, 15);
 var material = new THREE.MeshNormalMaterial( {color: 0x808000} );
 var sphere = new THREE.Mesh( forma, material );
 
