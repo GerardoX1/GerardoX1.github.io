8d3e3c90fac38791aeb555102478c9c03621ac90
GerardoX1 <yeral_way@hotmail.com>
2016-08-25 15:28:56 -0500

    Update MallaPoExtruccion.js

--- rv/MallaPoExtruccion.js
+++ rv/MallaPoExtruccion.js
@@ -12,3 +12,15 @@ var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} );
 var material = new THREE.MeshNormalMaterial();
 var malla = new Three.mesh( forma, material );
 malla.rotateX( Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizardor = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena,camara );    
