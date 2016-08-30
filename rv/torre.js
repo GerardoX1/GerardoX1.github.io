5ffbb08a300fd19215bd63abaef087be0c839fa3
GerardoX1 <yeral_way@hotmail.com>
2016-08-30 15:34:06 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,7 +1,7 @@
 var puntos = [];
 
 for ( var i = 0; i < 20; i ++ ) {
-	puntos.push(new THREE.Vector2(Math.sin( i*0.2 )*15 +50 ,(i - 5)*2) );
+	puntos.push(new THREE.Vector2(Math.sin( i*0.2 )*15 +50 ,i/2 );
 }
 
 var forma = new THREE.LatheGeometry(puntos);
