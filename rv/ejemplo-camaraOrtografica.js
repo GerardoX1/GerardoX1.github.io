e9f705998c912737f1bcf27b7c26af47e0979177
GerardoX1 <yeral_way@hotmail.com>
2016-09-06 15:30:17 -0500

    Update ejemplo-camaraOrtografica.js

--- rv/ejemplo-camaraOrtografica.js
+++ rv/ejemplo-camaraOrtografica.js
@@ -24,6 +24,7 @@ var escena = new THREE.Scene();
 escena.add(esfera1);
 escena.add(esfera2);
 escena.add(cubo);
+//
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight );
