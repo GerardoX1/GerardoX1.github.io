d8bcfa9ead294e97f7f7a6ece71598455744b124
GerardoX1 <yeral_way@hotmail.com>
2016-09-05 23:56:37 -0500

    Create estrella.js

---
+++ rv/estrella.js
@@ -0,0 +1,33 @@
+//conoceremos un constructor nuevo1
+var  figura = new THREE.Shape();
+//tenemos el nuevo objeto que genera la figura
+
+//figura
+//forma
+
+figura.moveTo(100, 0); 
+figura.lineTo(-80.9, -58.7); // genera una linea 40 40 desde esa localidad
+figura.lineTo(30.9, 95.1); // lo mismo pa este
+figura.lineTo(30.9, -95.1); // 
+figura.lineTo(-80.9, 58.7);
+figura.lineTo(1, 0);
+
+//comvertimos a una figura
+
+var forma = new THREE.ShapeGeometry(figura);
+var malla = new THREE.Mesh(forma);
+
+//agregamos la escena la malla la camara 
+// el renderizador el camvas 
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara =new THREE.PerspectiveCamera();
+camara.position.z= 100;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+											wi
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
