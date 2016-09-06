fc4b44ec085ee585fb6ef79e74024d6e4c9f6d53
GerardoX1 <yeral_way@hotmail.com>
2016-09-05 23:58:59 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -27,7 +27,7 @@ var camara =new THREE.PerspectiveCamera();
 camara.position.z= 100;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-											wi
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95 );
+		      
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
