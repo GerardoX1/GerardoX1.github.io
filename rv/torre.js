1816b75af06b5eb250a301cf0dcdae027a0e715c
GerardoX1 <yeral_way@hotmail.com>
2016-09-06 00:59:20 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -2,7 +2,7 @@ var escena = new THREE.Scene();
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 1000;
-camara.position.y = 300;
+camara.position.y = 200;
 
 
 var renderizador = new THREE.WebGLRenderer();
