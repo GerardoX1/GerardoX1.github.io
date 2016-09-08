7ce7d00a12324b5b663f0426c5e0320171cb2168
GerardoX1 <yeral_way@hotmail.com>
2016-09-08 15:34:09 -0500

    Create tablero.js

---
+++ rv/tablero.js
@@ -0,0 +1,45 @@
+var escena = new THREE.Scene();
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 1000;
+camara.position.y = 200;
+
+
+var renderizador = new THREE.WebGLRenderer();
+
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+
+      var geometry[][];
+      var material[][];
+      var cube[][];
+      
+      var PosX = 0;
+      var PosY = 0;
+
+
+for ( var i = 0; i < 8; i ++ ) {
+  for ( var j = 0; j < 8; j ++ ) {
+      
+      geometry[i][j] = new THREE.BoxGeometry( 10, 10, 3 );
+      
+      if((i+j)%2==0){
+      material[i][j] = new THREE.MeshBasicMaterial( {color: 0xffffff} ); }
+      else{
+      material[i][j] = new THREE.MeshBasicMaterial( {color: 0x888888} ); }
+  
+      cube[i][j] = new THREE.Mesh( geometry[i][j], material[i][j] );
+      
+      cube[i][j].position.x=PosX + 10;
+      cube[i][j].position.y=PosY + 10;
+      
+      PosX=PosX+10;
+      PosY=PosY+10;
+      
+      scene.add( cube[i][j] );
+      
+}
+}
+
+renderizador.render( escena,camara );    
