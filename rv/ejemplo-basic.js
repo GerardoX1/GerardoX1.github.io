2ac3454ea4d44d9e3104a266ab4d1faf04fb1217
GerardoX1 <yeral_way@hotmail.com>
2016-09-13 14:52:51 -0500

    Update ejemplo-basic.js

--- rv/ejemplo-basic.js
+++ rv/ejemplo-basic.js
@@ -5,7 +5,7 @@ var malla = new THREE.Mesh(forma,Material);
 var escena = new THREE.Scene();
 escena.add(malla);
 
-var camara = new THREE.Perspective Camera();
+var camara = new THREE.PerspectiveCamera();
 camara.position.z =5;
 
 var lienzo = document.getElementById("basicMaterial");
