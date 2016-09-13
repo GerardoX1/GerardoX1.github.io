0ab57a6ba164370ae09520c70053f43b0cdd7a20
GerardoX1 <yeral_way@hotmail.com>
2016-09-13 15:47:40 -0500

    Update Sombras.js

--- rv/Sombras.js
+++ rv/Sombras.js
@@ -22,9 +22,6 @@ var lienzo = document.getElementById("luzSinSombras");
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
                       window.innerHeight*.95);
-document.body.appendChild( renderizador.domElement );
-
-renderizador.setSize(600,600);
 
 renderizador.shadowMapEnabled = true;
 malla.castShadow =true;
