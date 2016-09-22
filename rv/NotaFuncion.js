d183a3f4411c0bf3c163cec629703f7c0af56556
GerardoX1 <yeral_way@hotmail.com>
2016-09-22 14:58:01 -0500

    Create NotaFuncion.js

---
+++ rv/NotaFuncion.js
@@ -0,0 +1,34 @@
+//Pre-Eventos funciones 
+//en JavaScrip las funciones son objetos de primer orden = "un valor" 
+
+//Dicotomia Dato/Proceso son cosas diferentes
+
+//Dato es el valor que trabajamos y la otra es el proceso que estoy realizando
+//la cuestion es que en java script no existe esto
+//LISP maneja las funciones como valores
+
+//Funciones pueden no ser un parametro Pueden no tener nombre
+
+//comenzamos
+
+//Primera forma de expresar una funcion "la clasica"
+
+function init(P){
+  var malla (new THREE.BoxGeometry(P,P,P), new THREE.MeshNormalMaterial());
+
+ escena= new THREE.Scene();
+ escena.add(malla);
+
+ camara = new THREE.PerspectiveCamera();
+ renderizador = new THREE.WedGLRender();
+
+renderizador.setSize(900,700);
+document.body.appendChild( renderizador.domElement );
+}
+
+
+//Siguiente forma "Tomamos como un valor la funcion antes generada"
+
+var main = function(P){
+renderizador.render(escena,camara);
+}
