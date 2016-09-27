6bf786f6c507446607845a65098a878a8ee783fe
GerardoX1 <yeral_way@hotmail.com>
2016-09-27 15:37:37 -0500

    Create Textura.js

---
+++ rv/Textura.js
@@ -0,0 +1,35 @@
+var TEXTURA = new Object();
+
+TEXTURA.retrollamada = funcion( textura ){
+    var material=new THREE.MeshBasicMaterial({map: texture});
+    TEXTURA.malla = new THREE.Mesh( new THREE.SphereGeometry(1),material);
+    TEXTURA.escena.add(TEXTURA.malla);
+    }
+    
+TEXTURA.setup = funcion(){
+
+    TEXTURA.escena = new THREE.Scene();
+    var cargador = new THREE.TextureLoader();
+    cargador.load("earth_atmos_2048.jpg",TEXTURE.retrollamada);
+    
+    TEXTURA.camara = new THREE.PesrpectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+    TEXTURA.camara,position.z = 5;
+    
+    var lienzo = document.getElementById("ejemplo-textura");
+    TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo,antialias: true});
+    TEXTURA.renderizador.setSize(600,600);
+    }
+ 
+TEXTURE.loop = function (){
+  requestAnimationFrame(TEXTURA.loop);
+  
+  if (TEXTURA.malla !== undedined ) {
+    TEXTURA.malla.rotateX( 0.01 );
+    TEXTURA.malla.rotateY( 0.01 );
+  }
+  TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara);
+  
+  TEXTURA.setup();
+  TEXTURA.loop();
+  
+  
