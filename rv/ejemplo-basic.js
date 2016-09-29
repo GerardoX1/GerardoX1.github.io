08fbea17015898992838c73408b8ae1593a50c16
GerardoX1 <yeral_way@hotmail.com>
2016-09-29 14:58:38 -0500

    Update ejemplo-basic.js

--- rv/ejemplo-basic.js
+++ rv/ejemplo-basic.js
@@ -1,3 +1,4 @@
+
 var forma = new THREE.SphereGeometry(1);
 var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
 var malla = new THREE.Mesh(forma,Material);
@@ -8,8 +9,10 @@ escena.add(malla);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z =5;
 
+
 var lienzo = document.getElementById("basicMaterial");
 var renderizador = new THREE.WebGLRenderer();
+
 
 renderizador.setSize(600,600);
 document.body.appendChild( renderizador.domElement );
