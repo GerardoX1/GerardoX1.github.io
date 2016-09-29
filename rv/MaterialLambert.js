f5952324320323ab80486d8d7b265fb8cb8c3e11
GerardoX1 <yeral_way@hotmail.com>
2016-09-29 15:03:03 -0500

    Update MaterialLambert.js

--- rv/MaterialLambert.js
+++ rv/MaterialLambert.js
@@ -9,7 +9,8 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z =5;
 
 var lienzo = document.getElementById("lambertMaterial");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+var renderizador = new THREE.WebGLRenderer();
 
 renderizador.setSize(600,600);
+document.body.appendChild( renderizador.domElement );
 renderizador.render(escena,camara);
