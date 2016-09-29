39606f094e9ed9a787f63484daf833792dd27cb8
GerardoX1 <yeral_way@hotmail.com>
2016-09-29 15:41:51 -0500

    Update Textura.js

--- rv/Textura.js
+++ rv/Textura.js
@@ -10,7 +10,7 @@ TEXTURA.setup = function(){
 
     TEXTURA.escena = new THREE.Scene();
     var cargador = new THREE.TextureLoader();
-    cargador.load("earth_atmos_2048.jpg",TEXTURE.retrollamada);
+    cargador.load("earth_atmos_2048.jpg",TEXTURA.retrollamada);
     
     TEXTURA.camara = new THREE.PesrpectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
     TEXTURA.camara,position.z = 5;
