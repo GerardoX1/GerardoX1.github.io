c605dea2f83333fe88460c32cb8f6ee53df8cbfc
GerardoX1 <yeral_way@hotmail.com>
2016-09-29 14:56:09 -0500

    Update ejemplo-basic.js

--- rv/ejemplo-basic.js
+++ rv/ejemplo-basic.js
@@ -9,10 +9,8 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.z =5;
 
 var lienzo = document.getElementById("basicMaterial");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+var renderizador = new THREE.WebGLRenderer();
 
 renderizador.setSize(600,600);
+document.body.appendChild( renderizador.domElement );
 renderizador.render(escena,camara);
-
-<canvas id="basicMaterial" width="600" height="600"></canvas>
-<script src="ejemplo-basicMaterial.js"></script>
