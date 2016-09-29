fcea44356c2a1d3b88c2f688da9f75b5c211585d
GerardoX1 <yeral_way@hotmail.com>
2016-09-29 15:56:17 -0500

    Update Textura.js

--- rv/Textura.js
+++ rv/Textura.js
@@ -1,7 +1,7 @@
 var TEXTURA = new Object();
 
 TEXTURA.retrollamada = function( textura ){
-    var material=new THREE.MeshBasicMaterial({map: texture});
+    var material=new THREE.MeshBasicMaterial({map: textura});
     TEXTURA.malla = new THREE.Mesh( new THREE.SphereGeometry(1),material);
     TEXTURA.escena.add(TEXTURA.malla);
     }
