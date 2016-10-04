7f8ffbc5b3ebea71fc191777e9e407e2a8a316a5
GerardoX1 <yeral_way@hotmail.com>
2016-10-04 15:13:50 -0500

    Create Constructor.js

---
+++ rv/Constructor.js
@@ -0,0 +1,49 @@
+function Arbol(){
+
+  var troncoForma = new THREE.CylinderGeometry(.25,.5,1);
+  var esferaForma = new THREE.SphereGeometry(.65);
+  esferaForma.translate(0,1,0);
+  
+  var troncoMalla = new THREE.Mesh(troncoForma);
+  var esferaMalla = new THREE.Mesh(esferaForma);
+  
+  var arbolForma = new THREE.Gerometry();
+  arbolForma.merge(troncoMalla.geometry,troncoMalla.matrix);
+  arbolForma.merge(esferaMalla.geometry,troncoMalla.matrix);
+  
+  var material = new THREE.MeshNormalMaterial();
+  this.malla = new THREE.Mesh(arbolForma,material);
+  }
+  
+  var CONSTRUCTOR = new Object();
+  
+  CONSTRUCTOR.setup = function(){
+    var arbol1 = new Arbol();
+    var arbol2 = new Arbol();
+    
+    arbol1.malla.position.x = -5;
+    arbol2.malla.position.x =  5;
+    
+    CONSTRUCTOR.camara = new THREE.PerspectiveCamera();
+    CONSTRUCTOR.camara.position.z = 20;
+    
+    var lienzo = document.getElementById("ejemplo-Constructor");
+    CONSTRUCTOR.renderizador = new THREE.WebGLRenderer();
+    
+    CONSTRUCTOR.renderizador.setSize( 600,600 );
+    document.body.appendChild( CONSTRUCTOR.renderizador.domElement );
+    
+   CONSTRUCTOR.escena = new THREE.Scene();
+   CONSTRUCTOR.escena.add(arbol1.malla);
+   CONSTRUCTOR.escena.add(arbol1.malla);
+  
+  }
+  
+  CONSTRUCTOR.loop = funcion(){
+    requestAnimationFrame( CONSTRUCTOR.loop );
+    CONSTRUCTOR.renderizador.render( CONSTRUCTOR.escena,CONSTRUCTOR.camara);
+    }
+    
+    CONSTRUCTOR.setup();
+    CONSTRUCTOR.loop();
+    
