733c3aad7606c508d0c49c2979e4491c1eecd1cc
GerardoX1 <yeral_way@hotmail.com>
2016-10-05 14:22:41 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -3,8 +3,6 @@ var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 100;
 
-
-
 var renderizador = new THREE.WebGLRenderer();
 
 renderizador.setSize( window.innerHeight*.95,
@@ -17,7 +15,6 @@ document.body.appendChild( renderizador.domElement );
       var geometry =new Array();
       var material= new Array();
       var cube= new Array();
-      var Tablero = new THREE.Geometry();
 
       for (i=0;i<iMax;i++) { geometry[i]=new Array();
           for (j=0;j<jMax;j++) { geometry[i][j]=0;}
@@ -30,6 +27,8 @@ document.body.appendChild( renderizador.domElement );
       for (i=0;i<iMax;i++) { cube[i]=new Array();
           for (j=0;j<jMax;j++) { cube[i][j]=0;}
       }
+
+var tablero=new THREE.Group();
 
 for ( var i = 0; i <= 7; i ++ ) {
   for ( var j = 0; j <= 7; j ++ ) {
@@ -45,11 +44,27 @@ for ( var i = 0; i <= 7; i ++ ) {
       
       cube[i][j].position.x=(j+1)*10;
       cube[i][j].position.y=(i+1)*10;
-      
-      Tablero.merge(cube[i][j].geometry,cube[i][j].matrix);
+      tablero.add(cube[i][j]);
 }
 }
-var tableroF=new THREE.Mesh(Tablero);
-escena.add( tableroF );
 
+
+     geometria1 = new THREE.BoxGeometry( 10, 90, 3 );
+     geometria2 = new THREE.BoxGeometry( 10, 90, 3 );
+     geometria3 = new THREE.BoxGeometry( 10, 90, 3 );
+     geometria4 = new THREE.BoxGeometry( 10, 90, 3 );
+     materialA = new THREE.MeshBasicMaterial( {color: 0x414344} );
+     materialB = new THREE.MeshBasicMaterial( {color: 0x37454F} );
+     
+     var BordeMalla1=new THREE.Mesh(geometria1,materialA);
+     var BordeMalla2=new THREE.Mesh(geometria2,materialB);
+     var BordeMalla3=new THREE.Mesh(geometria3,materialA);
+     var BordeMalla4=new THREE.Mesh(geometria4,materialB);
+     
+     BordeMalla1.
+
+
+
+escena.add( tablero );
+     
 renderizador.render( escena,camara );    
