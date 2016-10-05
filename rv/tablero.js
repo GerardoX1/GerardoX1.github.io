733b0d05b95f4b9ee8a986c996d8ae73f44ba1bc
GerardoX1 <yeral_way@hotmail.com>
2016-10-05 13:51:58 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,8 +1,8 @@
 var escena = new THREE.Scene();
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 1000;
-camara.position.y = 200;
+camara.position.z = 100;
+
 
 
 var renderizador = new THREE.WebGLRenderer();
