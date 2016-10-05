3aaa8c896870abb6153ca51551fc1877388705f0
GerardoX1 <yeral_way@hotmail.com>
2016-10-05 13:35:02 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -11,23 +11,46 @@ renderizador.setSize( window.innerHeight*.95,
                       window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement );
 
-      var geometry =new Array(new Array(),new Array());
-      var material= new Array(new Array(),new Array());
-      var cube= new Array(new Array(),new Array());
-      
+      var iMax = 8;
+      var jMax = 8;
+
+      var geometry =new Array();
+      var material= new Array();
+      var cube= new Array();
+
+      for (i=0;i<iMax;i++) {
+      geometry[i]=new Array();
+      for (j=0;j<jMax;j++) {
+      geometry[i][j]=0;
+      }
+      }
+
+      for (i=0;i<iMax;i++) {
+      material[i]=new Array();
+      for (j=0;j<jMax;j++) {
+      material[i][j]=0;
+      }
+      }
+
+      for (i=0;i<iMax;i++) {
+      cube[i]=new Array();
+      for (j=0;j<jMax;j++) {
+      cube[i][j]=0;
+      }
+      }
+
       var PosX = 0;
       var PosY = 0;
-
-
-for ( var i = 1; i < 8; i ++ ) {
-  for ( var j = 1; j < 8; j ++ ) {
+      
+for ( var i = 0; i <= 7; i ++ ) {
+  for ( var j = 0; j <= 7; j ++ ) {
       
       geometry[i][j] = new THREE.BoxGeometry( 10, 10, 3 );
       
       if((i+j)%2==0){
-      material[i][j] = new THREE.MeshBasicMaterial( {color: 0xffffff} ); }
+      material[i][j] = new THREE.MeshBasicMaterial( {color: 0x5B6C9D} ); }
       else{
-      material[i][j] = new THREE.MeshBasicMaterial( {color: 0x888888} ); }
+      material[i][j] = new THREE.MeshBasicMaterial( {color: 0xACD9FA} ); }
   
       cube[i][j] = new THREE.Mesh( geometry[i][j], material[i][j] );
       
