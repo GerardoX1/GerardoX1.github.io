7c9fc6924cb1a0a2c9900d40277c8c7989338434
GerardoX1 <yeral_way@hotmail.com>
2016-10-05 13:13:07 -0500

    Update TORREblanca.js

--- rv/TORREblanca.js
+++ rv/TORREblanca.js
@@ -1,93 +1,81 @@
-var escena = new THREE.Scene();
+//Torre (union por mallas)
+//para la primera parte definimos las figuras que generan nuestra pieza
 
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 1000;
-camara.position.y = 200;
+//tallo
 
-
-var renderizador = new THREE.WebGLRenderer();
-
-renderizador.setSize( window.innerHeight*.95,
-                      window.innerHeight*.95);
-document.body.appendChild( renderizador.domElement );
-
-var puntos = [];
+var tallo = [];
 for ( var i = 0; i < 30; i ++ ) {
-	puntos.push(new THREE.Vector2(
+	tallo.push(new THREE.Vector2(
 			Math.sin(i*0.2 )*15 +100 ,
 				(i - 5)*2) );
 }
 for ( i = 31; i < 40; i ++ ) {
-	puntos.push(new THREE.Vector2(
+	tallo.push(new THREE.Vector2(
 			60 ,
 				(i - 5)*2) );
 }
 for ( i = 41; i < 150; i ++ ) {
-	puntos.push(new THREE.Vector2(
+	tallo.push(new THREE.Vector2(
 		50,
 				(i - 5)*2) );
 }
 for ( i = 151; i < 170; i ++ ) {
-	puntos.push(new THREE.Vector2(
+	tallo.push(new THREE.Vector2(
 		80,
 				(i - 5)*2) );
 }
 
-puntos.push(new THREE.Vector2( 0 , 332 ));
+//conos superiores
 
-var forma = new THREE.LatheGeometry(puntos);
-var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var basse = new THREE.Mesh( forma,material );
-escena.add( basse );
+var conotorre1 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
+var conotorre2 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
+var conotorre3 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
+var conotorre4 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
+var conotorre5 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
+var conotorre6 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
 
-var geometry2 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material2 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder2 = new THREE.Mesh( geometry2,material2);
+//generamos las formas en el caso de los conos ya 
+//las tiene asi que no necesitamos mas que para //el tallo
 
-cylinder2.position.x=35;
-cylinder2.position.y=332;
-cylinder2.position.z=60.62;
-escena.add( cylinder2 )
+var forma = new THREE.LatheGeometry(tallo);
 
+//realizamos el movimiento de las piezas para //generar la torre como la deseamos
 
-var geometry3 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material3 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder3 = new THREE.Mesh( geometry3,material3 );
-cylinder3.position.x=70;
-cylinder3.position.y=332;
-cylinder3.position.z=0;
-escena.add( cylinder3 )
+conotorre1.translate(35,332,60.62);
+conotorre2.translate(70,332,0);
+conotorre3.translate(35,332,-60.62);
+conotorre4.translate(-35,332,-60.62);
+conotorre5.translate(-70,332,0);
+conotorre6.translate(-35,332,60.62);
 
-var geometry4 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material4 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder4 = new THREE.Mesh( geometry4, material4 );
-cylinder4.position.x=35;
-cylinder4.position.y=332;
-cylinder4.position.z=-60.62;
-escena.add( cylinder4 )
+//generamos sus respectivas mallas
 
-var geometry5 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material5 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder5 = new THREE.Mesh( geometry5,material5 );
-cylinder5.position.x=-35;
-cylinder5.position.y=332;
-cylinder5.position.z=-60.62;
-escena.add( cylinder5 )
+var talloMalla = new THREE.Mesh(forma);
+var cono1Malla = new THREE.Mesh(conotorre1);
+var cono2Malla = new THREE.Mesh(conotorre2);
+var cono3Malla = new THREE.Mesh(conotorre3);
+var cono4Malla = new THREE.Mesh(conotorre4);
+var cono5Malla = new THREE.Mesh(conotorre5);
+var cono6Malla = new THREE.Mesh(conotorre6);
 
-var geometry6 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material6 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder6 = new THREE.Mesh( geometry6,material6 );
-cylinder6.position.x=-70;
-cylinder6.position.y=332;
-cylinder6.position.z=0;
-escena.add( cylinder6 )
+//Creamos la geometria final
 
-var geometry7 = new THREE.CylinderGeometry( 16, 12, 30, 32 );
-var material7 = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-var cylinder7 = new THREE.Mesh( geometry7,material7);
-cylinder7.position.x=-35;
-cylinder7.position.y=332;
-cylinder7.position.z=60.62;
-escena.add( cylinder7 )
+var TorreF = new THREE.Geometry();
 
-renderizador.render( escena,camara );    
+//uniremos todas las mallas
+
+TorreF.merge(talloMalla.geometry,talloMalla.matrix);
+TorreF.merge(cono1Malla.geometry,cono1Malla.matrix);
+TorreF.merge(cono2Malla.geometry,cono2Malla.matrix);
+TorreF.merge(cono3Malla.geometry,cono3Malla.matrix);
+TorreF.merge(cono4Malla.geometry,cono4Malla.matrix);
+TorreF.merge(cono5Malla.geometry,cono5Malla.matrix);
+TorreF.merge(cono6Malla.geometry,cono6Malla.matrix);
+
+//Generaremos el objeto dandole material
+
+var material=new THREE.MeshBasicMaterial({color: 0x97E4EC});
+var torreMallaB=new THREE.Mesh(TorreF,material);
+
+//Escalamos la pieza para que entre en el tablero
+torreMallaB.scale.set(0.05,0.05,0.05)
