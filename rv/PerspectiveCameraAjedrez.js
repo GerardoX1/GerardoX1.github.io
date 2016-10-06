95a7d0f7762ec6e319d8204daec0390177eac58d
GerardoX1 <yeral_way@hotmail.com>
2016-10-06 15:24:12 -0500

    Update PerspectiveCameraAjedrez.js

--- rv/PerspectiveCameraAjedrez.js
+++ rv/PerspectiveCameraAjedrez.js
@@ -1,23 +1,34 @@
 //Camara perspective
 
 //Clonaremos las torres y las colocaremos conde deben de ir
+var materialBlanca1=new THREE.MeshLambertMaterial({color: 0xEEEED8, transparent: true, opacity: 1});
+var materialBlanca2=new THREE.MeshLambertMaterial({color: 0xEEEED8, transparent: true, opacity: 0.75});
 
-var torre1B= torreMallaB.clone();
+var torreBlancaMalla1=new THREE.Mesh(TorreFB,materialBlanca1);
+var torreBlancaMalla2=new THREE.Mesh(TorreFB,materialBlanca2);
+
+var materialNegra1=new THREE.MeshLambertMaterial({color: 0x000000, transparent: true, opacity: 0.5});
+var materialNegra2=new THREE.MeshLambertMaterial({color: 0x000000, transparent: true, opacity: 0.25});
+
+var torreNegraMalla2=new THREE.Mesh(TorreFN,materialNegra1);
+var torreNegraMalla2=new THREE.Mesh(TorreFN,materialNegra2);
+
+var torre1B= torreBlancaMalla1.clone();
 torre1B.position.x=10;
 torre1B.position.y=10;
 torre1B.position.z=3;
 
-var torre2B= torreMallaB.clone();
+var torre2B= torreBlancaMalla2.clone();
 torre2B.position.x=80;
 torre2B.position.y=10;
 torre2B.position.z=3;
 
-var torre3N= torreMallaN.clone();
+var torre3N= torreNegraMalla2.clone();
 torre3N.position.x=10;
 torre3N.position.y=80;
 torre3N.position.z=3;
 
-var torre4N= torreMallaN.clone();
+var torre4N= torreNegraMalla2.clone();
 torre4N.position.x=80;
 torre4N.position.y=80;
 torre4N.position.z=3;
