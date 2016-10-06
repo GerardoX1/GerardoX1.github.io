1d281beffd9cd3fbe3a2c447d970669411724db9
GerardoX1 <yeral_way@hotmail.com>
2016-10-06 15:28:30 -0500

    Update PerspectiveCameraAjedrez.js

--- rv/PerspectiveCameraAjedrez.js
+++ rv/PerspectiveCameraAjedrez.js
@@ -2,22 +2,22 @@
 
 //Clonaremos las torres y las colocaremos conde deben de ir
 
-var torre1B= torreBlancaMalla1.clone();
+var torre1B= torreMallaB.clone();
 torre1B.position.x=10;
 torre1B.position.y=10;
 torre1B.position.z=3;
 
-var torre2B= torreBlancaMalla2.clone();
+var torre2B= torreMallaB.clone();
 torre2B.position.x=80;
 torre2B.position.y=10;
 torre2B.position.z=3;
 
-var torre3N= torreNegraMalla2.clone();
+var torre3N= torreMallaN.clone();
 torre3N.position.x=10;
 torre3N.position.y=80;
 torre3N.position.z=3;
 
-var torre4N= torreNegraMalla2.clone();
+var torre4N= torreMallaN.clone();
 torre4N.position.x=80;
 torre4N.position.y=80;
 torre4N.position.z=3;
