69f99e90ad0f2d3880a00472508996db1beb84b6
GerardoX1 <yeral_way@hotmail.com>
2016-11-08 15:55:32 -0600

    Update EjercicioTexturaAjedrez.js

--- rv/EjercicioTexturaAjedrez.js
+++ rv/EjercicioTexturaAjedrez.js
@@ -1,22 +1,62 @@
-var torre1B= torreMallaB.clone();
-torre1B.position.x=10;
-torre1B.position.y=10;
-torre1B.position.z=3;
+//Puntos de la torre
+var setupdone=false;
+var textura1=false;
+var textura2=false;
+var textura3=false;
+var textura4=false;
+var textura5=false;
 
-var torre2B= torreMallaB.clone();
-torre2B.position.x=80;
-torre2B.position.y=10;
-torre2B.position.z=3;
+//CLONAMOS LAS PIEZAS
+var torre1= torreMallaB.clone();
+var torre2= torreMallaB.clone();
 
-var torre3N= torreMallaN.clone();
+
+var torre3= torreMallaN.clone();
 torre3N.position.x=10;
 torre3N.position.y=80;
 torre3N.position.z=3;
 
-var torre4N= torreMallaN.clone();
+var torre4= torreMallaN.clone();
 torre4N.position.x=80;
 torre4N.position.y=80;
 torre4N.position.z=3;
+
+        //Declaración del objeto
+var TEXTURA=new Object();
+//Declaración de las retrollamadas
+
+//Marmol blanco
+TEXTURA.retrollamadamblanco=function(textura){
+  var material=new THREE.MeshBasicMaterial({map:textura});
+  
+  TEXTURA.torre1B=new THREE.Mesh(torre1,material);
+  TEXTURA.torre2B=new THREE.Mesh(torre2,material);
+  
+  TEXTURA.torre1B.position.x=10;
+  TEXTURA.torre1B.position.y=10;
+  TEXTURA.torre1B.position.z=3;
+  
+  TEXTURA.torre2B.position.x=80;
+  TEXTURA.torre2B.position.y=10;
+  TEXTURA.torre2B.position.z=3;
+  
+textura1=true;
+TEXTURA.escena.add(TEXTURA.torre1B);
+TEXTURA.escena.add(TEXTURA.torre2B); 
+}
+
+
+TEXTURA.setup=function(){
+  
+  //Creación de la escena
+  TEXTURA.escena=new THREE.Scene();
+  
+  //Cargadores de las texturas
+  var cargador=new THREE.TextureLoader();
+  
+  //Configuración de las imagenes
+  cargador.load("MB.jpg",TEXTURA.retrollamadamblanco);
+}
 
 //Creamos luces
 //Creación de luces en la escena
@@ -24,6 +64,7 @@ torre4N.position.z=3;
 var luzPuntual=new THREE.PointLight(0xFFFF00);//AMARILLO
 var luzPuntual1=new THREE.PointLight(0xFF00FF);//ROSA
 var luzPuntual2=new THREE.PointLight(0x00FFFF);//CYAN
+
 //Posición de la iluminación
 luzPuntual.position.x=10;
 luzPuntual.position.y=90;
