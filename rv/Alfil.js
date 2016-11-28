23949b65c5ec27a1259943960f8d3dcf6ff24f6f
GerardoX1 <yeral_way@hotmail.com>
2016-11-28 17:05:04 -0600

    Update Alfil.js

--- rv/Alfil.js
+++ rv/Alfil.js
@@ -18,18 +18,31 @@ for ( i = 31; i < 40; i ++ ) {
 			60 ,
 				(i - 5)*2) );
 }
-for ( i = 41; i < 170; i ++ ) {
+for ( i = 41; i < 112; i ++ ) {
 	tallo.push(new THREE.Vector2(
 			Math.sin((i+40)*0.025 )*15 +40 ,
 				(i - 5)*2) );
 }
-/*
-for ( i = 161; i < 170; i ++ ) {
+
+for ( i = 112; i < 120; i ++ ) {
+	tallo.push(new THREE.Vector2(
+			50 ,
+				(i - 5)*2) );
+}
+
+
+for ( i = 120; i < 160; i ++ ) {
+	tallo.push(new THREE.Vector2(
+			Math.sin((i+40)*0.025 )*15 +40 ,
+				(i - 5)*2) );
+}
+
+for ( i = 160; i < 169; i ++ ) {
 	tallo.push(new THREE.Vector2(
 			60 ,
 				(i - 5)*2) );
 }
-*/
+
 
 tallo.push(new THREE.Vector2(0,171));
 
@@ -42,7 +55,7 @@ var Bolita = new THREE.SphereGeometry( 25, 32, 32 );
 var forma = new THREE.LatheGeometry(tallo);
 
 //Generacion de mallas y movimientos
-Bolita.translate(0,350,0);
+Bolita.translate(0,352,0);
 
 var talloMalla = new THREE.Mesh(forma);
 var BolitaMalla = new THREE.Mesh(Bolita);
