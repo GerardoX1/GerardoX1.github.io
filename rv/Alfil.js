13c65bbba153bb99923655ec46691c65d7733b38
GerardoX1 <yeral_way@hotmail.com>
2016-11-28 23:20:19 -0600

    Update Alfil.js

--- rv/Alfil.js
+++ rv/Alfil.js
@@ -1,67 +1,42 @@
-//Creacion del alfil
-
+//Creacion del Peon
 
 //definicion de las figuras que generan nuestra pieza
-
 //tallo
-
-
 var tallo = [];
 
 for ( var i = 0; i < 30; i ++ ) {
 	tallo.push(new THREE.Vector2(
-			Math.sin(i*0.2 )*15 +80 ,
+			Math.sin(i*0.2 )*15 +60 ,
 				(i - 5)*2) );
 }
 for ( i = 31; i < 40; i ++ ) {
 	tallo.push(new THREE.Vector2(
-			55 ,
+			50 ,
 				(i - 5)*2) );
 }
 for ( i = 41; i < 121; i ++ ) {
 	tallo.push(new THREE.Vector2(
-			Math.sin((i+60)*0.025 )*15 +40 ,
+			Math.sin((i+40)*0.025 )*15 +40 ,
 				(i - 5)*2) );
 }
-////
-for ( i = 121; i < 126; i ++ ) {
+for ( i = 121; i < 125; i ++ ) {
 	tallo.push(new THREE.Vector2(
-			45 ,
+			40 ,
 				(i - 5)*2) );
-}
-
-for ( i = 126; i < 131; i ++ ) {
-	tallo.push(new THREE.Vector2(
-			30 ,
-				(i - 5)*2) );
-}
-
-for ( i = 131; i < 136; i ++ ) {
-	tallo.push(new THREE.Vector2(
-			35 ,
-				(i - 5)*2) );
-}
-
-tallo.push(new THREE.Vector2(0,262));
-
+	
+tallo.push(new THREE.Vector2(0,240));
 
 //gorrito
-
-var Cono = new THREE.ConeGeometry( 25, 65, 32 );
-var bolita=new THREE.SphereGeometry( 11, 32, 32 );
+var bolita=new THREE.SphereGeometry( 25, 32, 32 );
 
 //generamos las formas el tallo
 
 var forma = new THREE.LatheGeometry(tallo);
 
 //Generacion de mallas y movimientos
-
-Cono.translate(0,295,0);
-bolita.translate(0,332,0);
+bolita.translate(0,255,0);
 
 var talloMalla = new THREE.Mesh(forma);
-
-var ConoMalla = new THREE.Mesh(Cono);
 var bolitaMalla = new THREE.Mesh(bolita);
 
 //Creamos la Geometria Final
@@ -71,6 +46,4 @@ var Alfil = new THREE.Geometry();
 //Unimos las mallas
 
 Alfil.merge(talloMalla.geometry,talloMalla.matrix);
-
-Alfil.merge(ConoMalla.geometry,ConoMalla.matrix);
 Alfil.merge(bolitaMalla.geometry,bolitaMalla.matrix);
