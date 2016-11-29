a9a837154fe3a901ea78f1af54f3f0187a056b1e
GerardoX1 <yeral_way@hotmail.com>
2016-11-29 14:09:57 -0600

    Update Alfil.js

--- rv/Alfil.js
+++ rv/Alfil.js
@@ -19,22 +19,39 @@ tallo.push(new THREE.Vector2(0,70));
 
 //Busto de caballo
 
-var Cono = new THREE.ConeGeometry( 25, 65, 32 );
-var bolita=new THREE.SphereGeometry( 11, 32, 32 );
+var PCaballo = new THREE.Shape();
 
-//generamos las formas el tallo
+PCaballo.moveTo( -55,70 );
+PCaballo.lineTo(-92,-115);
+PCaballo.lineTo(-49,-265);
+PCaballo.lineTo(50,-320);
+PCaballo.lineTo(121,-320);
+PCaballo.lineTo(142,-336);
+PCaballo.lineTo(155,-325);
+PCaballo.lineTo(152,-305);
+PCaballo.lineTo(175,-283);
+PCaballo.lineTo(253,-204);
+PCaballo.lineTo(259,-197);
+PCaballo.lineTo(265,-182);
+PCaballo.lineTo(230,-139);
+PCaballo.lineTo(181,-159);
+PCaballo.lineTo(118,-153);
+PCaballo.lineTo(87,-194);
+PCaballo.lineTo(87,-146);
+PCaballo.lineTo(184,-20);
+PCaballo.lineTo(174,67);
+PCaballo.lineTo(-55,70);
+PCaballo.lineTo( -55,70);
+
+//generamos la forma de la base
 
 var forma = new THREE.LatheGeometry(tallo);
+var BCaballo = new THREE.ShapeGeometry( PCaballo );
 
 //Generacion de mallas y movimientos
 
-Cono.translate(0,295,0);
-bolita.translate(0,332,0);
-
 var talloMalla = new THREE.Mesh(forma);
-
-var ConoMalla = new THREE.Mesh(Cono);
-var bolitaMalla = new THREE.Mesh(bolita);
+var CaballoMalla = new THREE.Mesh( BCaballo);
 
 //Creamos la Geometria Final
 
@@ -43,6 +60,5 @@ var Alfil = new THREE.Geometry();
 //Unimos las mallas
 
 Alfil.merge(talloMalla.geometry,talloMalla.matrix);
+Alfil.merge(CaballoMalla.geometry,CaballoMalla.matrix);
 
-Alfil.merge(ConoMalla.geometry,ConoMalla.matrix);
-Alfil.merge(bolitaMalla.geometry,bolitaMalla.matrix);
