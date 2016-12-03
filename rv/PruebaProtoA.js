0b8d7ef41bdb180f27760c74487c4200a5be0dba
GerardoX1 <yeral_way@hotmail.com>
2016-12-03 09:37:34 -0600

    Update PruebaProtoA.js

--- rv/PruebaProtoA.js
+++ rv/PruebaProtoA.js
@@ -384,8 +384,9 @@ CONSTRUCTOR.TexturaSetup= function(){
     //cargador.load("sp-152a-vidrio-spectrum.jpg",function(textura){ CONSTRUCTOR.PiezaBlanca = textura;});
     
     cargador.load("marmol blanco 2.jpg",function(textura){ CONSTRUCTOR.PiezaBlanca = textura;});
-    cargador.load("bea17cc676ac235c0cbd140b58dbb9c0.jpg",function(textura){ CONSTRUCTOR.PiezaNegra = textura;});
-   
+    //cargador.load("bea17cc676ac235c0cbd140b58dbb9c0.jpg",function(textura){ CONSTRUCTOR.PiezaNegra = textura;});
+    cargador.load("sp-152a-vidrio-spectrum.jpg",function(textura){ CONSTRUCTOR.PiezaNegra = textura;});
+
     cargador.load("bea17cc676ac235c0cbd140b58dbb9c0.jpg",function(textura){ CONSTRUCTOR.marmolNegro = textura;});
     cargador.load("12910526-tela-brillante-textura-de-fondo-Foto-de-archivo.jpg",function(textura){ CONSTRUCTOR.marmolBlanco = textura;});
 
